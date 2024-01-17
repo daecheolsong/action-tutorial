fb447392aa64f63830544afb46fdfbbdad5d61d4
daecheolsong <eocjf8905@naver.com>
2024-01-18 00:25:04 +0900

    hello controller test 추가

--- src/test/java/com/example/gitaction/GitActionApplicationTests.java
+++ src/test/java/com/example/gitaction/GitActionApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.gitaction;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(GitActionApplication.class)
 class GitActionApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
     }
 
 }
